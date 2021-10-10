b710d660e707719e335c0342b6622464f54d3df9
giangnguyen1992 <tgn1992@hotmail.de>
2021-10-10 22:40:02 +0200

    added callback functions

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-import fetch from "node-fetch";
+const request = require("request");
+const geocode = require("./utils/geocode");
 
-const url =
-  "http://api.weatherstack.com/current?access_key=8fc72416788ce768561b821ae1b1d499&query=New%20York";
-const fetchRequest = async (url) => {
-  try {
-    const data = await fetch(url);
-    const dataJSON = await data.json();
-    console.log(dataJSON.current?.temperature);
-  } catch (e) {
-    console.log("muuuuh", e);
-  }
-};
+// const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-112'
 
-fetchRequest(url);
+// request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service!')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location')
+//     } else {
+//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+//     }
+// })
+
+geocode("Boston", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
+});
